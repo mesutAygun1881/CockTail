23104af9b4f2c09aa70459747b749cf47e05f789
mesutAygun1881 <mesutaygun35@gmail.com>
2021-05-11 14:06:48 +0300

    first commit

---
+++ Cocktail/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Cocktail
+//
+//  Created by Mesut Aygün on 10.05.2021.
+//
+
+import Foundation
---
+++ Cocktail/Shows.swift
@@ -0,0 +1,8 @@
+//
+//  Shows.swift
+//  Cocktail
+//
+//  Created by Mesut Aygün on 10.05.2021.
+//
+
+import Foundation
